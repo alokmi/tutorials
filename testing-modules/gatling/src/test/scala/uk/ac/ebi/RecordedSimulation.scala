12232c19f948862a74034c8ee7b679801310baeb
Alok Mishra <amishra@ebi.ac.uk>
2021-01-26 06:38:30 +0000

    added post method

--- testing-modules/gatling/src/test/scala/uk/ac/ebi/RecordedSimulation.scala
+++ testing-modules/gatling/src/test/scala/uk/ac/ebi/RecordedSimulation.scala
@@ -12,21 +12,23 @@ class RecordedSimulation extends Simulation {
   val dbSNPIds = csv("dbsnp.csv") circular
 
   val hgvss = csv("hgvs.csv") circular
-  
+
   val pdbeaccessions = csv("pdbe_accessions.csv") circular
-  
+
   val accessions = csv("accessions.csv") circular
-  
+
   val geneNames = csv("gene_names.csv") circular
-  
+
+  val dbsnpjsons = csv("dbsnpjson.csv") circular
+
   val conf = ConfigFactory.load()
   val url = conf.getString("add.pepvep.base.url")
   val concurrentConsumer = conf.getInt("add.pepvep.concurrent.user")
 
-  val httpProtocol = http
-    .baseUrl(url)
+  val httpProtocol = http.baseUrl(url)
     .inferHtmlResources(BlackList(""".*\.css""", """.*\.js""", """.*\.ico"""), WhiteList())
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptHeader("application/json,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .contentTypeHeader("application/json")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0")
@@ -37,26 +39,24 @@ class RecordedSimulation extends Simulation {
     .feed(accessions)
     .feed(geneNames)
     .feed(pdbeaccessions)
+    .feed(dbsnpjsons)
     .forever {
-      exec(http("gene_request")
-      .get("/genomic/${geneName}"))
-    .pause(5)
-    .exec(http("protein_request")
-      .get("/protein/${accession}"))
-    .pause(5)
-    .exec(http("pdbe_request")
-      .get("/structure/${pdbeaccession}"))
-    .pause(4)
-    .exec(http("dbsnp_request")
-      .get("/dbSNP/${dbsnp}?species=homo_sapiens"))
-    .pause(2)
-    .exec(http("hgvs_request")
-      .get("/hgvs/${hgvs}?species=homo_sapiens"))
-    .pause(2)
-    .exec(http("region_request")
-      .get("/region/14%3A89993420-89993421?allele=A%2FG&species=homo_sapiens"))
-    .pause(1)
-  }
+      exec(http("gene_request_bulk")
+        .get("/genomic/${geneName}"))
+        .exec(http("protein_request_bulk")
+          .get("/protein/${accession}"))
+        .exec(http("pdbe_request_bulk")
+          .get("/structure/${pdbeaccession}"))
+        .exec(http("dbsnp_request_bulk")
+          .get("/dbSNP/${dbsnp}?species=homo_sapiens"))
+        .exec(http("hgvs_request_bulk")
+          .get("/hgvs/${hgvs}?species=homo_sapiens"))
+        .exec(http("region_request_bulk")
+          .get("/region/14%3A89993420-89993421?allele=A%2FG&species=homo_sapiens"))
+      exec(http("dbsnp_post_request_bulk")
+        .post("/dbSNP")
+        .body(StringBody("${dbsnpjson}")).asJson)
+    }
 
   setUp(scn.inject(atOnceUsers(concurrentConsumer))).protocols(httpProtocol).maxDuration(conf.getInt("add.run.maxDuration") minutes)
 }
